34c772b88474bfbe72976683c8ef056e4f6303f3
Linus Ericsson <oscarlinusericsson@gmail.com>
2024-03-09 23:41:28 +0100

    working parser for file

---
+++ src/tilltabell_v3.clj
@@ -0,0 +1,16 @@
+(ns tilltabell-v3
+  "Det ser ut som att den nya exporten är betydligt bättre
+  \"Arkivhandlingar\" förekommer två gånger, men det är ett lösbart problem.
+"
+  (:require [clojure.java.io :as io]
+            [clojure.data.csv :as csv]
+            [clojure.string :as str]
+            [instaparse.core :as insta]))
+
+(def postläsare (insta/parser (io/file "posterv3-working.bnf")))
+
+
+
+(def alla (postläsare (slurp "../allaplatser1.txt")))
+
+(first alla)
